f497432f296980136b2119da234f11c912ae19ce
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-02-06 09:14:55 +0300

    Created 'Count the Number of Consistent Strings' by A2SV Hub

---
+++ 06-Feb-2025/Count the Number of Consistent Strings 245650.py
@@ -0,0 +1,12 @@
+# Problem: Count the Number of Consistent Strings - https://leetcode.com/problems/count-the-number-of-consistent-strings/
+
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        count=0
+        for i in words:
+            for j in i :
+                if j not in allowed:
+                    break
+            else:
+                   count+=1
+        return count
