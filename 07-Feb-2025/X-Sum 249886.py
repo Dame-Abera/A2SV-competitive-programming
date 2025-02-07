0f366007f14c00889fcf1b40e68a72898404fc9f
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-02-07 16:08:55 +0300

    Created 'X-Sum' by A2SV Hub

---
+++ 07-Feb-2025/X-Sum 249886.py
@@ -0,0 +1,38 @@
+# Problem: X-Sum - https://codeforces.com/problemset/problem/1676/D
+
+from collections import defaultdict
+n=int(input())
+for i in range(n):
+    row,col=map(int,input().split())
+    chess=[]
+    for j in range(row):
+        line=list(map(int,input().split()))
+        chess.append(line)
+    defdec=defaultdict(int)
+    for rs in range(row):
+        for cs in range(col):
+            r,c=rs,cs
+            tot=0
+            while r<len(chess) and c<len(chess[0]):
+                tot+=chess[r][c]
+                c+=1
+                r+=1
+            r,c=rs,cs
+            while c>=0 and r>=0:
+                tot+=chess[r][c]  
+                c-=1
+                r-=1
+            r,c=rs,cs
+            while c<len(chess[0])  and r>=0:
+                tot+=chess[r][c]
+                c+=1
+                r-=1
+            r,c=rs,cs
+            while r<len(chess) and c>=0:
+                tot+=chess[r][c]
+                c-=1
+                r+=1
+            defdec[(rs,cs)]=tot-3*chess[rs][cs]
+         
+    temp=list(defdec.values())
+    print(max(temp))
