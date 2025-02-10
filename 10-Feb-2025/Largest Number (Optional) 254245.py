68fab766cefae80318d34d1c5387201113747299
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-02-10 21:09:48 +0300

    Created 'Largest Number (Optional)' by A2SV Hub

---
+++ 10-Feb-2025/Largest Number (Optional) 254245.py
@@ -0,0 +1,14 @@
+# Problem: Largest Number (Optional) - https://leetcode.com/problems/largest-number/
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for  idx,val  in enumerate(nums):
+            nums[idx]=str(val)
+        def  helper(n1,n2):
+            if n1+n2>n2+n1:
+                return -1
+            else:
+                return 1
+        ans=sorted(nums,key=cmp_to_key(helper)) 
+        print(ans)
+        return str(int("".join(ans)))
