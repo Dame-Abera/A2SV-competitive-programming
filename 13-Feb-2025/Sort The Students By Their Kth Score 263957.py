9f6a72ba205a9a7790f566635845a331525634e2
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-02-14 00:20:24 +0300

    Created 'Sort The Students By Their Kth Score' by A2SV Hub

---
+++ 13-Feb-2025/Sort The Students By Their Kth Score 263957.py
@@ -0,0 +1,12 @@
+# Problem: Sort The Students By Their Kth Score - https://leetcode.com/problems/sort-the-students-by-their-kth-score/
+
+class Solution:
+    def sortTheStudents(self, score: List[List[int]], k: int) -> List[List[int]]:
+            # for i  in  range(len(score)):
+            #     for  j in range(len(score)-1):
+            #         if score[j][k]<score[j+1][k]:
+            #             score[j],score[j+1]=score[j+1],score[j]
+            # return score
+            score.sort(key=lambda s:s[k], reverse=True)
+            return score
+            
