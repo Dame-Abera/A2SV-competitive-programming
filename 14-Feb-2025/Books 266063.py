f087fecf1fe15f49ca1d70d91cb217d8890d49ed
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-02-14 17:17:31 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Feb-2025/Books 266063.py
@@ -0,0 +1,17 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n,b=map(int,input().split())
+arr=list(map(int,input().split()))
+book=0
+l=0
+m=0
+for i in range(len(arr)):
+    book+=arr[i]
+    while  book>b:
+        book-=arr[l]
+        l+=1
+      
+    m=max(m,i-l+1)   
+
+print(m)     
+
