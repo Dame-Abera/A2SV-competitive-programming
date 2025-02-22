fe9f7dd3f33b6313819f9cd72c71cc594160f3d0
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-02-22 19:34:21 +0300

    Created 'Good Subarrays' by A2SV Hub

---
+++ 22-Feb-2025/Good Subarrays 280361.py
@@ -0,0 +1,22 @@
+# Problem: Good Subarrays - https://codeforces.com/problemset/problem/1398/C
+
+from collections import defaultdict
+t=int(input())
+for _ in range(t):
+    arr=[]
+    n=int(input())
+    strs=input()
+    for i  in strs:
+        arr.append(int(i))
+    prefix=[0] 
+    
+    for j in arr:
+        prefix.append(prefix[-1]+j)
+    deck=defaultdict(int)  
+    res=0
+    
+    for k in range(len(prefix)):
+        if prefix[k]-k in deck:
+            res+=deck[prefix[k]-k]
+        deck[prefix[k]-k]+=1
+    print(res)    
