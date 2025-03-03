2a16d21e551532eedbca0e1d9b4943fe2fb48c50
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-03-03 18:13:07 +0300

    Created 'Largest Perimeter Triangle' by A2SV Hub

---
+++ 03-Mar-2025/Largest Perimeter Triangle 289665.py
@@ -0,0 +1,16 @@
+# Problem: Largest Perimeter Triangle - https://leetcode.com/problems/largest-perimeter-triangle/
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        area=0
+        nums.sort(reverse=True)
+        
+        for i in range(len(nums)-2):
+            if nums[i+1]+nums[i+2]>nums[i]:
+                return  nums[i+2]+nums[i+1]+nums[i]
+        return 0         
+
+        
+
+
+        
