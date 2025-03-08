9d5ee6331e0fa18c84b66d6fb8c32d722d7f0ae1
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-03-08 21:19:06 +0300

    Created 'F - The Last Problem' by A2SV Hub

---
+++ A2SV G6 - Round #5 08-Mar-2025/F - The Last Problem 298126.py
@@ -0,0 +1,27 @@
+# Problem: F - The Last Problem - https://codeforces.com/gym/591928/problem/F
+
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+prefix = [0]
+for i in range(n):
+    prefix.append(prefix[-1] + a[i] * b[i])
+ans = prefix[n] 
+for i in range(n):
+    prod = a[i] * b[i] 
+    l, r = i-1, i +1
+    while l >= 0 and r < n:
+        prod +=a[r]* b[l]  
+        prod +=a[l]* b[r]
+        ans = max(ans, prod + prefix[l] + (prefix[-1] - prefix[r+1]))
+        l -=1
+        r +=1    
+    prod = 0
+    l, r = i, i+1
+    while l >= 0 and r < n:
+        prod +=a[r]* b[l]  
+        prod +=a[l]* b[r]
+        ans = max(ans, prod + prefix[l] + (prefix[-1] - prefix[r+1]))
+        l -=1
+        r +=1
+print(ans)
