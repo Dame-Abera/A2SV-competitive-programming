17b58a450104f8b4a34083d5b7603a5421704763
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-03-11 13:53:43 +0300

    Created 'E - Minimum Subsequence' by A2SV Hub

---
+++ A2SV G6 - Round #6 11-Mar-2025/E - Minimum Subsequence 302261.py
@@ -0,0 +1,42 @@
+# Problem: E - Minimum Subsequence - https://codeforces.com/gym/594077/problem/E
+
+from collections import deque
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    b = input()
+    
+    stack = deque()
+    count = 0
+    res = []
+    z=[]
+    one=[]
+    c=0
+    for i  in b:
+        
+        if i=="0":
+            if one:
+                now=one.pop()
+                z.append(now)
+                res.append(now)
+
+                
+            else:
+                c+=1
+                z.append(c)  
+                res.append(c)
+                  
+        else:  
+            if z:
+                 now=z.pop()
+                 one.append(now)
+                 res.append(now)
+                
+            else:
+                c+=1
+                one.append(c)
+                res.append(c)
+    print(c)       
+    print(*res) 
+    
