7b87602da0a4521786e62d55511f4619e8e13e34
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-03-11 13:55:19 +0300

    Created 'E - From S To T' by A2SV Hub

---
+++ A2SV G6 - Round #1 11-Mar-2025/E - From S To T 302264.py
@@ -0,0 +1,25 @@
+# Problem: E - From S To T - https://codeforces.com/gym/585107/problem/E
+
+n=int(input())                 
+for i in range(n):
+    s=input()
+    t=input()
+    p=input()
+    arr=list(p)     
+    temp=[]
+    j=0
+    for  char  in t:
+        if j<len(s)  and char==s[j]:
+            temp.append(char)
+            j+=1
+        elif char in arr:
+                temp.append(char)
+                arr.remove(char)
+    if j<len(s):
+         print("NO") 
+    else:     
+        if "".join(temp)==t:
+           print("YES")
+        else:
+            print("NO")
+    
