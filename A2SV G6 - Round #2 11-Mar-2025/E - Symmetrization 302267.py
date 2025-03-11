01d448593320cda121b9a46e4830b1980d4ecd17
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-03-11 14:00:38 +0300

    Created 'E - Symmetrization' by A2SV Hub

---
+++ A2SV G6 - Round #2 11-Mar-2025/E - Symmetrization 302267.py
@@ -0,0 +1,24 @@
+# Problem: E - Symmetrization - https://codeforces.com/gym/586960/problem/E
+
+s=int(input())
+mat=[]
+temp=[]
+ones=0
+count=0
+for _ in range(s):
+    n=int(input())
+    mat=[]
+    count=0
+    for s in range(n):
+        line=input()
+        mat.append([int(t) for t in line])
+    for i in range((n+1)//2):
+        for j in range(i,n-i-1):
+            ones=0
+            ones+=mat[i][j]
+            ones+=mat[j][n-1-i]
+            ones+=mat[n-1-i][n-j-1]
+            ones+=mat[n-1-j][i]  
+            count+=min(ones,4-ones)
+    print(count)
+
