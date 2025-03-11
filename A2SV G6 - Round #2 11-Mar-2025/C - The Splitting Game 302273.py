55ac9a5127137112e2d6f3a7a5f1ac90d14d7426
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-03-11 14:01:55 +0300

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 11-Mar-2025/C - The Splitting Game 302273.py
@@ -0,0 +1,23 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import defaultdict
+n=int(input())
+l=0
+res=0
+for i in range(n):
+    h=int(input())
+    string=input()
+    m1=defaultdict(int)
+    m2=defaultdict(int)
+    res=0
+    for j in string:
+        m1[j]+=1
+    for k in string:
+        m2[k]+=1
+       
+        if m1[k]==1:
+            del m1[k]
+        else:
+            m1[k]-=1
+        res=max(res,len(m1)+len(m2))
+    print(res)
