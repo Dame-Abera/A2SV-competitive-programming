78b10ab06d17bbf21f6a79cd651e603658fb7abc
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-03-19 14:03:08 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 19-Mar-2025/F - Binary Substrings with Exactly k Ones 312270.py
@@ -0,0 +1,18 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+from collections import defaultdict
+k=int(input())
+arr=input()
+deck=defaultdict(int)
+prefix=[0]
+for i in range(len(arr)):
+   
+    prefix.append(prefix[-1]+int(arr[i]))  
+ 
+ans=0 
+for i  in prefix:
+    if i-k  in deck:
+        ans+=deck[i-k]
+    deck[i]+=1
+print(ans)
+
