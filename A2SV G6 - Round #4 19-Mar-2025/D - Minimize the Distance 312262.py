608c4673034de153afda59eaa9fb2c594c52e2a1
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-03-19 13:53:38 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 19-Mar-2025/D - Minimize the Distance 312262.py
@@ -0,0 +1,41 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+
+import sys
+import os
+import math
+import itertools
+import collections
+import heapq
+from functools import lru_cache
+from bisect import bisect_left, bisect_right
+
+# Fast input
+input = sys.stdin.read
+def inp(): return sys.stdin.readline().strip()
+def inps(): return sys.stdin.readline().split()
+def inpi(): return int(sys.stdin.readline())
+def inpl(): return list(map(int, sys.stdin.readline().split()))
+
+# Output optimization
+def printl(arr, sep=" "): 
+    sys.stdout.write(sep.join(map(str, arr)) + "\n")
+
+
+INF = float('inf')
+MOD = 10**9 + 7
+
+def solve():
+    n=inpi()
+    arr=inpl()
+    arr.sort()
+    print(arr[(n-1)//2])
+    
+
+# Boilerplate
+if __name__ == "__main__":
+    sys.setrecursionlimit(10**6)
+    if os.path.exists("input.txt"):  # Debugging with file input
+        sys.stdin = open("input.txt", "r")
+        sys.stdout = open("output.txt", "w")
+    solve()
