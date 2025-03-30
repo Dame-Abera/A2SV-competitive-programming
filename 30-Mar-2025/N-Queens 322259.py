26773ae0c773aa5b8a61c24b0c8f8321402c1652
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-03-30 23:42:43 +0300

    Created 'N-Queens' by A2SV Hub

---
+++ 30-Mar-2025/N-Queens 322259.py
@@ -0,0 +1,32 @@
+# Problem: N-Queens - https://leetcode.com/problems/n-queens/description/
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board=[["."]*n for i   in range(n)]
+
+        print(board)
+        res=[]
+        main1=set()
+        main2=set()
+        cols=set()
+        def backtrack(row):
+            if row==n:
+                res.append(["".join(i) for i  in board])
+                return 
+            for col  in range(n):
+                if col in   cols or col-row in main1 or col+row in main2:
+                    continue
+                main1.add(col-row)  
+                main2.add(col+row) 
+                cols.add(col) 
+                board[row][col]="Q"
+                backtrack(row+1)
+                main1.remove(col-row)  
+                main2.remove(col+row) 
+                cols.remove(col)
+                board[row][col]="."
+
+
+        backtrack(0)
+        return res
+        
