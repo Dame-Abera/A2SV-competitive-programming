c9dbef5c7c7269b85881843d7f5eb55eaa87c1bf
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-04-11 23:32:24 +0300

    Created 'Count Number of Complete Components' by A2SV Hub

---
+++ 11-Apr-2025/Count Number of Complete Components 332351.py
@@ -0,0 +1,28 @@
+# Problem: Count Number of Complete Components - https://leetcode.com/problems/count-the-number-of-complete-components/
+
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        deck=defaultdict(list)
+        par=0 
+        for a,b  in edges: 
+            deck[a].append(b)
+            deck[b].append(a)
+        def dfs(node,visited,vg):
+            visited.add(node)
+            vg.add(node)
+            for i in deck[node]:
+                if i not in visited:
+                   dfs(i,visited,vg)
+        vg=set()    
+        for  i in range(n):
+            visited=set()
+            if i not in vg:
+                dfs(i,visited,vg)
+                a=len(visited)
+                print(visited,vg)       
+                for k in visited:
+                    if len(deck[k])!=a-1:
+                        break
+                else:
+                    par+=1        
+        return par 
