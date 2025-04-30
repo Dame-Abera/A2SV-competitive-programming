ed9cf60f832f6a231add3cb2dd244f8420ff1d0c
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-04-30 17:51:21 +0300

    Created 'Minimum Operations to Exceed Threshold Value II' by A2SV Hub

---
+++ 30-Apr-2025/Minimum Operations to Exceed Threshold Value II 345552.py
@@ -0,0 +1,14 @@
+# Problem: Minimum Operations to Exceed Threshold Value II - https://leetcode.com/problems/minimum-operations-to-exceed-threshold-value-ii/
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        # p=heaq.heappop(nums)
+        count=0
+        while nums[0]<k:
+            x=heapq.heappop(nums)
+            y=heapq.heappop(nums)
+            heapq.heappush(nums,(min(x, y) * 2 + max(x, y)) )
+            count+=1
+        return count    
+            
