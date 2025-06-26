38dfce7499d419b53d3a2a364590cc492ade5375
Dame Abera <161621839+Dame-Abera@users.noreply.github.com>
2025-06-26 23:18:39 +0300

    Created 'Single Number' by A2SV Hub

---
+++ 26-Jun-2025/Single Number 370953.py
@@ -0,0 +1,11 @@
+# Problem: Single Number - https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result=0
+        for i in nums:
+           result^=i
+        return result 
+
+            
+        
